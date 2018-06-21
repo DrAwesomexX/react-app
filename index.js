f328a7d2cce4abbf4d6a7a61ebd270f614ac0119
Manav <manavbajaj1997@gmail.com>
2018-06-21 15:15:36 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hello: 'there' });
+  res.send({ hi: 'there' });
 });
 
 const PORT = process.env.PORT || 5000;
