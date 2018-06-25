e6cdfb4a32442454c84be721d28f9442d37b7c42
Manav <manavbajaj1997@gmail.com>
2018-06-25 11:10:44 +0530

    finished outh flow

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys.js');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User.js');
 
+mongoose.connect(keys.mongoURI);
+require('./services/passport.js');
 var app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes.js')(app);
 
 const PORT = process.env.PORT || 5000;
 
